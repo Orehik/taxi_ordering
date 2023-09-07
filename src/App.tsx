d26d91abc680fd247665cba1db74d6109dec1990
Oleg Demchenko <d.oleg-19953@mail.ru>
2023-09-07 09:42:58 +0300

    form commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { YMaps, Map } from "react-yandex-maps";
+import mapStile from "./App.module.scss"
+import Form from './Form'
 
 function App() {
 
   return (
     <YMaps query={{ apikey: import.meta.env.VITE_YA_MAPS_API_KEY }}>
-      <div>
-        <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
-      </div>
+        <Form />
+        <Map className={mapStile.map} defaultState={{ center: [55.75, 37.57], zoom: 11 }} />
     </YMaps>
-  )
+
+)
 }
 
 export default App
