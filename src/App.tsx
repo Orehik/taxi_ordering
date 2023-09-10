234548b6154789e7d7612a3321e8ea8286ed70e4
Oleg Demchenko <d.oleg-19953@mail.ru>
2023-09-10 18:32:53 +0300

    minimal positioning of elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import Autocomplete from "./component/Form/Autocomplete";
 import YandexMap from "./component/Map/YandexMap"
 import TaxiList from "./component/TaxiList/TaxiList";
+import divStyle from "./App.module.scss"
 
 function App() {
 
   return (
-    <>
-    <h1>Детали заказа</h1>
+    <div className={divStyle.parent}>
+      <div className={divStyle.orderBlock}>
+      <h1>Детали заказа</h1>
       <div>Откуда <Autocomplete /></div>
+      </div>
+      <div className={divStyle.cardBlock}>
       <YandexMap />
       <TaxiList/>
-    </>
+      </div>
+    </div>
 )
 }
 
--- src/component/TaxiList/TaxiList.tsx
+++ src/component/TaxiList/TaxiList.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setTaxiAC } from "../../redux/actions/reduserAC";
 import TaxiItem from "../TaxiItem/TaxiItem"
 import { selectTaxiList } from "../../redux/selectors/taxi";
+import listStyle from "./TaxiList.module.scss"
 
 const TaxiList = () => {
   const dispatch = useDispatch();
@@ -14,7 +15,7 @@ const TaxiList = () => {
   }, [dispatch])
 
   return (
-    <div>
+    <div className={listStyle.list}>
       {taxiList.map((item) => <TaxiItem key={item.crew_id} {...item} hex_color={COLOR_NAME[item.car_color] ?? COLOR_NAME["белый"]}/>)}
     </div>
   );
